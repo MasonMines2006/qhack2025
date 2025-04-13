86687800e311f7db608d51159910ccfc38de1793
Conor McHaney <cmchaney@unc.edu>
2025-04-13 06:55:53 -0400

    expanded basic app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
 import streamlit as st
 import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="Quantum Intro", layout="centered")
+
+st.title("A basic introduction to quantum concepts and computing")
+
+st.markdown("---")
+
+st.header("Welcome!")
+st.write("This is a simple web app to help you explore quantum computing concepts.")
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
-df
+st.dataframe(df)
