c3629e2375ebb1a0cec6cdd8636f9666a3f9ea1f
Conor McHaney <cmchaney@unc.edu>
2025-04-13 07:53:55 -0400

    made honmepage

--- app.py
+++ app.py
@@ -2,18 +2,26 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="Quantum Intro", layout="centered")
-
-st.title("A Baisc Introduction to Quantum Concepts and Computing :sunglasses:")
+st.title("Carolina Quantum Hacks 2025!")
 
 st.markdown("---")
 
-st.header("Welcome!")
-st.write("This is a simple web app to help you explore quantum computing concepts.")
+st.header("*Welcome*! :wave:")
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+st.write("This is a simple web app to help you explore quantum concepts and their significance.")
 
-st.dataframe(df)
+st.markdown("---")
+
+st.header("*Why Quantum?* :thinking_face:")
+
+st.write("Quantum computing is revolutionizing how we solve complex problems by unlocking computational power far beyond classical limits — enabling breakthroughs in areas like drug discovery, where it can simulate molecules too complex for today’s supercomputers; cybersecurity, where it challenges current encryption and pushes the development of quantum-safe cryptography; and global logistics, where it can optimize routes and supply chains faster and more efficiently than ever before.")
+
+st.markdown("---")
+
+st.header("*Quantum Circuit Visualization* :art:")
+
+st.write("Here is a bit of art formed from the result of measuring quantum circuits:")
+
+st.markdown("---")
+
+st.header("*Check Out Other Interesting Quantum Concepts!* :fire:")
